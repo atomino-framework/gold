6e06133b49450a3f7b0c62f40897ed7b5c5a4b01
Gergely Laborci <gergely@laborci.hu>
2022-02-14 15:29:58 +0100

    authorization added

--- src/AuthApi.php
+++ src/AuthApi.php
@@ -8,19 +8,24 @@ use Atomino\Mercury\Responder\Api\Api;
 use Atomino\Mercury\Responder\Api\Attributes\Auth;
 use Atomino\Mercury\Responder\Api\Attributes\Route;
 use Symfony\Component\HttpFoundation\Response;
+use function Atomino\debug;
 
 abstract class AuthApi extends Api {
 
-	public function __construct(private SessionAuthenticator $authenticator) { }
+	public function __construct(protected SessionAuthenticator $authenticator) { }
 
 	abstract public function getAuthenticated(): Entity|null;
 	abstract public function getUserName(Entity $user): string;
 	abstract public function getUserAvatar(Entity $user): string|null;
+	abstract public function authorize(Entity $user): bool;
 
 	#[Route(self::POST, '/get')]
 	public final function POST_get() {
 		$user = $this->getAuthenticated();
-		if (is_null($user)) return null;
+		if (is_null($user) || !$this->authorize($user)) {
+			$this->logout();
+			return null;
+		}
 		return [
 			"id"     => $user->id,
 			"name"   => $this->getUserName($user),
@@ -30,12 +35,17 @@ abstract class AuthApi extends Api {
 	}
 
 	#[Route(self::POST, '/login')]
-	#[Auth(false)]
 	public final function POST_login() {
+		$this->logout();
 		$login = $this->data->get('login');
 		$password = $this->data->get('password');
 		if (!$this->authenticator->login($login, $password)) {
 			$this->setStatusCode(Response::HTTP_UNAUTHORIZED);
+			return false;
+		}
+		if (!$this->authorize($this->getAuthenticated())) {
+			$this->setStatusCode(Response::HTTP_FORBIDDEN);
+			$this->logout();
 			return false;
 		}
 		return true;
@@ -44,7 +54,11 @@ abstract class AuthApi extends Api {
 	#[Route(self::POST, '/logout')]
 	#[Auth]
 	public final function POST_logout() {
-		$this->authenticator->logout($this->getResponse());
+		$this->logout();
+	}
+
+	protected function logout() {
+		if (!is_null($this->getAuthenticated())) $this->authenticator->logout($this->getResponse());
 	}
 
 }
