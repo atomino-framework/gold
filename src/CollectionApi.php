ed189bc2b900088f118047e0f37a65f5dfdde408
Gergely Laborci <gergely@laborci.hu>
2022-02-19 10:44:15 +0100

    collection api has order

--- src/CollectionApi.php
+++ src/CollectionApi.php
@@ -6,6 +6,8 @@ use Atomino\Mercury\Responder\Api\Attributes\Route;
 
 class CollectionApi extends AbstractApi {
 
+	public $order = [["id","ASC"]];
+
 	protected function filter(string $search, int|null $contextId = null): Filter { return Filter::where("id=$1", $search); }
 	protected function label(Entity $item): string { return $item->id; }
 
@@ -13,18 +15,18 @@ class CollectionApi extends AbstractApi {
 	public final function POST_search(): array {
 		$id = $this->data->get("id", null);
 		$search = $this->data->get("search");
-		$items = ($this->entity)::search($this->filter($search, $id))->collect();
+		$items = ($this->entity)::search($this->filter($search, $id))->order(...$this->order)->collect();
 		$result = [];
-		foreach ($items as $item) $result[$item->id] = $this->label($item);
+		foreach ($items as $item) $result[] = ["value"=>$item->id, "label"=>$this->label($item)];
 		return $result;
 	}
 
 	#[Route("POST", "/get")]
 	public final function POST_get(): array {
 		$value = $this->data->get("value");
-		$items = ($this->entity)::search(Filter::where(($this->entity)::id($value)))->collect();
+		$items = ($this->entity)::search(Filter::where(($this->entity)::id($value)))->order(...$this->order)->collect();
 		$result = [];
-		foreach ($items as $item) $result[$item->id] = $this->label($item);
+		foreach ($items as $item) $result[] = ["value"=>$item->id, "label"=>$this->label($item)];
 		return $result;
 	}
 
